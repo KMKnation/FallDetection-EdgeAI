170e16f596c2a106a242342d0914e208161dca06
mayur.kanojiya <kanojiyamayur@gmail.com>
2020-11-28 19:59:39 +0530

    fix: troubleshoot message added

--- send_mail.py
+++ send_mail.py
@@ -18,19 +18,28 @@ class SendMail(object):
         # Create a secure SSL context
         self.context = ssl.create_default_context()
         self.server = smtplib.SMTP_SSL("smtp.gmail.com", self.port, context=self.context)
-        self.server.login(self.sender, password)
+        try:
+            self.server.login(self.sender, password)
+        except Exception as err:
+            print(
+                'Turn Allow less secure apps to ON from following link. Be aware that this makes it easier for others to gain access to your account.')
+            print('https://myaccount.google.com/lesssecureapps')
 
     def send_alert(self, body, fall_type):
         try:
 
-            message = """From: From Person <{}>
-            To: To Person <{}>
+            message = """From: <{}>
+            To:  <{}>
             Subject: Alert | Fall Detected | {}
 
             {}.
             """.format(self.sender, receivers[0], fall_type, body)
+
             self.server.sendmail(self.sender, receivers, message)
             print("Successfully sent email")
         except smtplib.SMTPException as err:
+            print(
+                'Turn Allow less secure apps to ON from following link. Be aware that this makes it easier for others to gain access to your account.')
+            print('https://myaccount.google.com/lesssecureapps')
             print("Error: unable to send email Reason: {}".format(err))
             exit(0)
